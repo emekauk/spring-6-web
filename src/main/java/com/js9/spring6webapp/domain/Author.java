dc35fc550321e4069933f34ca70ff1244946e164
EMEKA-UKWUOMA <ukwuoma@systemspecs.com.ng>
2023-12-10 00:46:27 +0100

    Added implementations for equals and hashcode

--- src/main/java/com/js9/spring6webapp/domain/Author.java
+++ src/main/java/com/js9/spring6webapp/domain/Author.java
@@ -3,6 +3,7 @@ package com.js9.spring6webapp.domain;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -57,5 +58,31 @@ public class Author {
 
     public void setBooks(Set<Book> books) {
         this.books = books;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
